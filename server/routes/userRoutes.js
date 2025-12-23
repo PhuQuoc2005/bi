50a108d5eed51f285c9c7dc2c8dc0099e823036a
Nguyen Minh Phung <nguyenminhphung1307@gmail.com>
2025-12-23 15:44:01 +0700

    add api createOrder

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/userController.js";
+import { login, logout, signup } from "../controllers/UserController.js";
 
 const router = express.Router();
 
